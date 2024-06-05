feb5dfc13da81c01223020da5dbdf86b2009e3b9
Orinugoori <bigbangok@naver.com>
2024-06-05 10:58:46 +0900

    정수 처리 추가

--- app/src/main/java/com/example/secondweek/Calculator.kt
+++ app/src/main/java/com/example/secondweek/Calculator.kt
@@ -2,13 +2,17 @@ package com.example.secondweek
 
 class Calculator {
 
-    fun add(numbers : List<Double>): String{
-        return if (numbers.isEmpty()){
+    fun add(numbers: List<Number>): String {
+        return if (numbers.isEmpty()) {
             "0"
-        } else{
-            val sum = numbers.sum()
-            sum.toString()
+        } else {
+            val sum = numbers.sumOf { it.toDouble() }
+            if (sum % 1.0 == 0.0) {
+                sum.toInt().toString()
+            } else {
+                sum.toString()
+            }
         }
-    }
 
+    }
 }
--- app/src/main/java/com/example/secondweek/StringAnalyze.kt
+++ app/src/main/java/com/example/secondweek/StringAnalyze.kt
@@ -2,14 +2,15 @@ package com.example.secondweek
 
 class StringAnalyze {
 
-    fun filterNumbers(currentInput: String): List<Double> {
+    fun filterNumbers(currentInput: String): List<Number> {
         return currentInput.split("+","-","*","/")
-            .mapNotNull { it.toDoubleOrNull() }
+            .mapNotNull { it.toDoubleOrNull() ?: it.toIntOrNull() }
     }
+
 
     fun filterOperators(currentInput: String): List<String> {
         return currentInput.split(" ")
-            .filter{it.toDoubleOrNull()==null}
+            .filter{it.toDoubleOrNull()==null && it.toIntOrNull() == null }
     }
 
 
