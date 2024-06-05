da3e77887eea4120350153451131f269f117a4b9
Orinugoori <bigbangok@naver.com>
2024-06-05 12:31:45 +0900

    버튼 누를때 이미지 변경되게 수정

--- app/src/main/java/com/example/secondweek/MainActivity.kt
+++ app/src/main/java/com/example/secondweek/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.secondweek
 
 import android.os.Bundle
-import android.widget.ImageButton
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
